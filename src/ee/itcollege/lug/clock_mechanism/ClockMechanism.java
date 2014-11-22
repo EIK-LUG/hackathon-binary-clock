1af3b505688f1a6f426daacd31bb8aa5b4ab672d
AleksandrBabykin <alexbabykin@gmail.com>
2014-11-22 14:40:12 +0200

    Implement tick

--- src/ee/itcollege/lug/clock_mechanism/ClockMechanism.java
+++ src/ee/itcollege/lug/clock_mechanism/ClockMechanism.java
@@ -1,13 +1,20 @@
 package ee.itcollege.lug.clock_mechanism;
 
 
+import ee.itcollege.lug.display.DisplayInterface;
+import ee.itcollege.lug.display.cli.CLIDisplay;
 
 /**
  * Created by chuck-norris on 22.11.14.
  */
 public class ClockMechanism {
+    private DisplayInterface display = new CLIDisplay();
 
     public long getTime() {
         return 1;
+    }
+
+    public void tick() {
+
     }
 }
