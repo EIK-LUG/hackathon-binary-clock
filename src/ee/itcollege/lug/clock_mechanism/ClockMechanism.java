2b9b7ee09503fbb7b2729d1b38616598a23de0fc
Timofei <timo@tdl.ee>
2014-11-22 14:39:29 +0200

    Tried to do something w/ Clockmechanism

--- src/ee/itcollege/lug/clock_mechanism/ClockMechanism.java
+++ src/ee/itcollege/lug/clock_mechanism/ClockMechanism.java
@@ -1,13 +1,21 @@
 package ee.itcollege.lug.clock_mechanism;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by chuck-norris on 22.11.14.
  */
 public class ClockMechanism {
 
-    public long getTime() {
-        return 1;
+    private long getTime() {
+
+        Calendar cal = Calendar.getInstance();
+        cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println(sdf.format(cal.getTime()));
+
+        //TODO RETURN
     }
 }
